e6af1e4cf31799d6c8a217b3042699c264c58427
Ilyas <ilyas.ilimbaev@yandex.ru>
2021-03-27 19:29:37 +0500

    New commit

--- docs/js/main.js
+++ docs/js/main.js
@@ -56,6 +56,62 @@ $(document).ready(function() {
             }
         });
     }
+    //form validate
+    $('.contact-form').validate ({
+        rules: {
+            email: {
+                required: true,
+                email: true
+            },
+            subject: {
+                required: true
+            },
+            message: {
+                required: true
+            }
+        },
+        messages: {
+            email: {
+                required: 'Введите email',
+                email: 'отсутствует символ @'
+            },
+            subject: {
+                required: 'Введите тему сообщения'
+            },
+            message: {
+                required: 'Введите сообщение'
+            }
+        }
+    })
+    //dots
+    // Create page-nav dots
+	$("#page-nav").onePageNav({
+		currentClass: "active",
+		changeHash: false,
+		scrollSpeed: 750,
+		scrollThreshold: 0.5,
+		filter: "",
+		easing: "swing",
+		begin: function () {},
+		end: function () {},
+		scrollChange: function ($currentListItem) {},
+	});
+    //отправка данных
+	function ajaxFormSubmit() {
+		let string = $(".contact-form").serialize();
+		$.ajax({
+			type: "POST",
+			url: "php/mail.php",
+			data: string,
+			success: function (html) {
+				$(".contact-form").slideUp(800);
+				$("#answer").html(html);
+			},
+		});
+		return false;
+	}
+
+    return false;
     //изменение размера блока 
     // const BigElementBlock = document.querySelector('.portfolio-works__item--big');
     // $('.control-active').click(function(){
