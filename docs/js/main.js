607d03538c57646e1b399c550a00012e2640fb95
Ilyas <ilyas.ilimbaev@yandex.ru>
2021-04-20 16:24:17 +0500

    Additing js

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
+    	// ==== link-relocation-delay ====
+	$(function () {
+		$("a[href^='#']").click(function () {
+			var _href = $(this).attr("href");
+			$("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+			return false;
+		});
+	});
     const toggleMenu = document.querySelector('.toggle-menu');
     const mobileMenu = document.querySelector('.mobile-menu');
     const overlay = document.querySelector('#overlay');
